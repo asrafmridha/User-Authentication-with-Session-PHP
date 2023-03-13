02c3b17e31aed909152159ecc1b09827c9d9ca5f
asraf-mridha <asrafmridha@gmail.com>
2023-03-13 20:05:28 +0600

    Conditional apply for from show or hidden

--- auth.php
+++ auth.php
@@ -49,24 +49,31 @@ if (isset($_POST['username']) && $_POST['password']) {
         ?>
         </div>
 
-        <div class="col col-60 col-offset-20 mt-3">
-            <form method="POST" action="/ostad_php_project/login_session/auth.php">
-                <div class="form-group">
-                    <label for="exampleInputEmail1">UserName</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Username" name="username">
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
-                </div>
-                <div class="form-group form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-        </div>
+        <?php if ($_SESSION['loggedin'] == false) { ?>
+
+            <div class="col col-60 col-offset-20 mt-3">
+                <form method="POST" action="/ostad_php_project/login_session/auth.php">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">UserName</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Username" name="username">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
+                    </div>
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+
+        <?php } else { ?>
+            <button type="submit" class="btn btn-danger">logout</button> <?php } ?>
+
+
     </div>
 </body>
 
